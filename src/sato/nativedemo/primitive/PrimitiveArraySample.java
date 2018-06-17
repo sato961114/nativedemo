0eaadaad4dd8d5211d24d2bdc8cb0ad44468cc14
sato <sato@sato-PC>
2018-06-17 22:15:31 +0900

    javadoc.

--- src/sato/nativedemo/primitive/PrimitiveArraySample.java
+++ src/sato/nativedemo/primitive/PrimitiveArraySample.java
@@ -1,15 +1,73 @@
 package sato.nativedemo.primitive;
 
+/**
+ * example of primitive arrays.<br>
+ * 
+ * native implementation does not vary the argument.<br>
+ * this example includes String type, that is not primitive, because String type is often used.<br>
+ */
 public class PrimitiveArraySample {
 
+	/**
+	 * pass and recieve boolean array.
+	 * @param array boolean array.
+	 * @return boolean array.
+	 */
 	public native boolean[] booleanSample(boolean[] array);
+	
+	/**
+	 * pass and recieve byte array.
+	 * @param array byte array.
+	 * @return byte array.
+	 */
 	public native byte[] byteSample(byte[] array);
+	
+	/**
+	 * pass and recieve char array.
+	 * @param array char array.
+	 * @return char array.
+	 */
 	public native char[] charSample(char[] array);
+	
+	/**
+	 * pass and recieve short array.
+	 * @param array short array.
+	 * @return short array.
+	 */
 	public native short[] shortSample(short[] array);
+	
+	/**
+	 * pass and recieve int array.
+	 * @param array int array.
+	 * @return int array.
+	 */
 	public native int[] intSample(int[] array);
+	
+	/**
+	 * pass and recieve long array.
+	 * @param array long array.
+	 * @return long array.
+	 */
 	public native long[] longSample(long[] array);
+	
+	/**
+	 * pass and recieve float array.
+	 * @param array float array.
+	 * @return float array.
+	 */
 	public native float[] floatSample(float[] array);
+	
+	/**
+	 * pass and recieve double array.
+	 * @param array double array.
+	 * @return double array,
+	 */
 	public native double[] doubleSample(double[] array);
 
+	/**
+	 * pass and recieve String array.
+	 * @param array String array.
+	 * @return String array.
+	 */
 	public native String[] stringSample(String[] array);
 }
--- src/sato/nativedemo/primitive/PrimitiveSample.java
+++ src/sato/nativedemo/primitive/PrimitiveSample.java
@@ -1,16 +1,78 @@
 package sato.nativedemo.primitive;
 
+/**
+ * example of primitive values.
+ * 
+ * native implementation does not vary the argument.<br>
+ * this example includes String type, that is not primitive, because String type is often used.<br>
+ */
 public class PrimitiveSample {
 
+	/**
+	 * pass and recieve boolean value.
+	 * @param value boolean.
+	 * @return boolean.
+	 */
 	public native boolean booleanSample(boolean value);
+	
+	/**
+	 * pass and recieve byte value.
+	 * @param value byte.
+	 * @return byte.
+	 */
 	public native byte byteSample(byte value);
+	
+	/**
+	 * pass and recieve char value.
+	 * @param value char.
+	 * @return char.
+	 */
 	public native char charSample(char value);
+	
+	/**
+	 * pass and recieve short value.
+	 * @param value short.
+	 * @return short.
+	 */
 	public native short shortSample(short value);
+	
+	/**
+	 * pass and recieve int value.
+	 * @param value int.
+	 * @return int.
+	 */
 	public native int intSample(int value);
+	
+	/**
+	 * pass and recieve long value.
+	 * @param value long.
+	 * @return long.
+	 */
 	public native long longSample(long value);
+	
+	/**
+	 * pass and recieve float value.
+	 * @param value float.
+	 * @return float.
+	 */
 	public native float floatSample(float value);
+	
+	/**
+	 * pass and recieve double value.
+	 * @param value double.
+	 * @return double.
+	 */
 	public native double doubleSample(double value);
+	
+	/**
+	 * void example.
+	 */
 	public native void voidSample();
 
+	/**
+	 * pass and recieve String value.
+	 * @param value String.
+	 * @return String.
+	 */
 	public native String stringSample(String value);
 }
